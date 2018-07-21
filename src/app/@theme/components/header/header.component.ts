7c81f6b76c8ffd3affa37a1a7223d62a7710efa8
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2018-07-21 16:44:01 +0200

    customized header added daterangepicker

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -3,6 +3,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
+import { NgxDateRangePickerOptions } from 'ngx-daterangepicker';
+
 
 @Component({
   selector: 'ngx-header',
@@ -18,6 +20,8 @@ export class HeaderComponent implements OnInit {
 
   userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
 
+  options: NgxDateRangePickerOptions;
+
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               private userService: UserService,
@@ -27,6 +31,39 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.userService.getUsers()
       .subscribe((users: any) => this.user = users.daimler);
+
+    this.options = {
+      theme: 'green', //  customized in styles.scss, color changed manually in module ($cyan in colors.sass)
+      labels: ['Start', 'End'],
+      menu: [
+        {alias: 'td', text: 'Today', operation: '0d'},
+        {alias: 'tm', text: 'This Month', operation: '0m'},
+        {alias: 'lm', text: 'Last Month', operation: '-1m'},
+        {alias: 'tw', text: 'This Week', operation: '0w'},
+        {alias: 'lw', text: 'Last Week', operation: '-1w'},
+        {alias: 'ty', text: 'This Year', operation: '0y'},
+        {alias: 'ly', text: 'Last Year', operation: '-1y'},
+        {alias: 'ny', text: 'Next Year', operation: '+1y'},
+        {alias: 'lyt', text: 'Last year from today', operation: '-1yt'},
+      ],
+      dateFormat: 'YYYY-MM-DD',
+      outputFormat: 'DD-MM-YYYY',
+      startOfWeek: 0,
+      outputType: 'object',
+      locale: 'es',
+      date: {
+        from: {
+          year: 2017,
+          month: 3,
+          day: 5,
+        },
+        to: {
+          year: 2017,
+          month: 3,
+          day: 6,
+        },
+      },
+    };
   }
 
   toggleSidebar(): boolean {
