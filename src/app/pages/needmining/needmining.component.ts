796f257467ad2e5d5c51eaf6329fc074a72e7c86
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2018-07-22 21:11:06 +0200

    added needmining page with basic charts

---
+++ src/app/pages/needmining/needmining.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'ngx-sentiment',
+  styleUrls: ['./needmining.component.scss'],
+  templateUrl: './needmining.component.html',
+})
+export class NeedminingComponent implements OnDestroy {
+  ngOnDestroy(): void {
+  }
+
+}
