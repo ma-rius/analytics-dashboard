6f48cc3417bf918ee01b943099b94f9c8c78008d
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2018-07-22 21:13:51 +0200

    added personality page with basic charts

---
+++ src/app/pages/personality/personality.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'ngx-sentiment',
+  styleUrls: ['./personality.component.scss'],
+  templateUrl: './personality.component.html',
+})
+export class PersonalityComponent implements OnDestroy {
+  ngOnDestroy(): void {
+  }
+
+}
