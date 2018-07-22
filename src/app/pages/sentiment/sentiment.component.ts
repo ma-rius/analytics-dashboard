a3516c380b249a34542e5ec44653143d0dfc7230
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2018-07-22 21:14:09 +0200

    added sentiment page with basic charts

---
+++ src/app/pages/sentiment/sentiment.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'ngx-sentiment',
+  styleUrls: ['./sentiment.component.scss'],
+  templateUrl: './sentiment.component.html',
+})
+export class SentimentComponent implements OnDestroy {
+  ngOnDestroy(): void {
+  }
+
+}
---
+++ src/app/pages/sentiment/sentiment.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { SentimentComponent } from './sentiment.component';
+import { EchartsComponent } from './echarts/echarts.component';
+import { EchartsLineComponent } from './echarts/echarts-line.component';
+import { EchartsPieComponent } from './echarts/echarts-pie.component';
+import { EchartsBarComponent } from './echarts/echarts-bar.component';
+import { EchartsMultipleXaxisComponent } from './echarts/echarts-multiple-xaxis.component';
+import { EchartsAreaStackComponent } from './echarts/echarts-area-stack.component';
+import { EchartsBarAnimationComponent } from './echarts/echarts-bar-animation.component';
+import { EchartsRadarComponent } from './echarts/echarts-radar.component';
+
+
+const components = [
+  EchartsComponent,
+  EchartsLineComponent,
+  EchartsPieComponent,
+  EchartsBarComponent,
+  EchartsMultipleXaxisComponent,
+  EchartsAreaStackComponent,
+  EchartsBarAnimationComponent,
+  EchartsRadarComponent,
+];
+
+
+@NgModule({
+  imports: [
+    ThemeModule,
+    NgxEchartsModule,
+  ],
+  declarations: [
+    SentimentComponent, ...components,
+  ],
+})
+export class SentimentModule { }
