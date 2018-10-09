b2cce966da3af45bd33fcac6934027b15083431b
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2018-10-09 14:08:53 +0200

    switched to final API for bar chart

--- src/app/@core/data/dashboard-bar.service.ts
+++ src/app/@core/data/dashboard-bar.service.ts
@@ -1,22 +1,27 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Selection} from '../model/selection';
 import {Observable} from 'rxjs/Observable';
+import {formatDate} from '@angular/common';
 
 @Injectable()
 export class DashboardBarService {
-  private apiUrl = 'http://needminer.com/api/machine-learning/clustering?text=benziner+machen+krank&domain=emobility';
+  // private apiUrl = 'http://129.13.133.28/api/get-tweets-by-time-and-limit/?start=1538431405176&end=1538435256197';
+  private apiUrl = 'http://129.13.133.28/api/bar-chart-by-time-and-subject/';
 
+  // private apiUrl = 'https://cpp80390de.execute-api.eu-central-1.amazonaws.com/prod';
   constructor(private http: HttpClient) {
   }
 
-  // todo solve Access-Control issue
+
   getData(selection: Selection): Observable<any> {
-    return this.http.get(this.apiUrl);
-
-
-    // console.log(JSON.stringify(a));
-    // return a;
-    // return [300, 52, 200, 334, 390, 330, 220];
+    const params = new HttpParams()
+      .set('start', new Date(selection.dateFrom).valueOf().toString())
+      .set('end', new Date(selection.dateTo).valueOf().toString())
+      .set('ai', '1')
+      .set('blockchain', '1')
+      .set('emobility', '1')
+      .set('iot', '1');
+    return this.http.get(this.apiUrl, {params: params});
   }
 }
