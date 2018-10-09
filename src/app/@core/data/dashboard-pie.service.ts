ecee8de0c2e0be5a3646c52e0a982b6f9bd38262
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2018-10-09 17:05:30 +0200

    performance improvement: only load the necessary data

--- src/app/@core/data/dashboard-pie.service.ts
+++ src/app/@core/data/dashboard-pie.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Selection} from '../model/selection';
 import {Observable} from 'rxjs/Observable';
-import {formatDate} from '@angular/common';
 
 @Injectable()
 export class DashboardPieService {
@@ -15,13 +14,17 @@ export class DashboardPieService {
 
 
   getData(selection: Selection): Observable<any> {
+    const ai = 1 ? selection.domains.artificial_intelligence : 0;
+    const blockchain = 1 ? selection.domains.blockchain : 0;
+    const emobility = 1 ? selection.domains.e_mobility : 0;
+    const iot = 1 ? selection.domains.internet_of_things : 0;
     const params = new HttpParams()
       .set('start', new Date(selection.dateFrom).valueOf().toString())
       .set('end', new Date(selection.dateTo).valueOf().toString())
-      .set('ai', '1')
-      .set('blockchain', '1')
-      .set('emobility', '1')
-      .set('iot', '1');
+      .set('ai', ai.toString())
+      .set('blockchain', blockchain.toString())
+      .set('emobility', emobility.toString())
+      .set('iot', iot.toString());
     return this.http.get(this.apiUrl, {params: params});
   }
 }
